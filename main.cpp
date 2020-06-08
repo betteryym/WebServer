a06fe6752d3599e024cb96499a97fd286fe47d7f
betteryym <395871036@qq.com>
2020-06-08 23:56:55 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
-#include <iostream>
-int main(){
-    std::cout << "hello" << std::cout;
+#include "config.h"
+
+int main(int argc, char* argv[]){
+    //需要修改的数据库信息， 登陆名， 密码， 库名
+    string user = "yym";
+    string passwd = "123";
+    string databasename = "yourdb";
+
+    //命令行解析
+    Config config;
+    config.parse_arg(argc, argv);
+
+    WebServer server;
+
+    //初始化
+    server.init(config.PORT, user, passwd, databasename, config.LOGWrite, config.SQLVerify,
+                        config.OPT_LINGER, config.TRIGMode, config.sql_num, config.thread_num,
+                        config.close_log, config.actor_model);
+
+    //日志
+    server.log_write();
+
+    //数据库
+    server.sql_pool();
+
+    //线程池
+    server.thread_pool();
+
+    //监听
+    server.eventListen();
+
+    //运行
+    server.eventLoop();
+
     return 0;
 }
