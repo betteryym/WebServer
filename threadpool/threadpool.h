1352f37fc28f49b991bece5b45cbaf976edea2c8
yym <395871036@qq.com>
2020-06-10 11:55:03 +0800

    threadpool.h 11.54

---
+++ threadpool/threadpool.h
@@ -0,0 +1,67 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <list>
+#include <cstdio>
+#include <exception>
+#include <pthread.h>
+#include "../lock/locker.h"
+#include "../CGImysql/sql_connection_pool.h"
+
+template <typename T>
+class threadpool{
+public:
+    /*thread_number 是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    threadpool(int actor_model, connection_pool* connPool, int thread_number = 8, int max_request = 10000);
+    ~threadpool();
+    bool append(T* request, int state);
+    bool append_p(T* request);
+
+private:
+    /*工作线程运行的函数，不断从工作队列中取出任务并执行*/
+    static void* worker(void* art);
+    void run();
+
+private:
+    int m_thread_number;         //线程池中的线程数
+    int m_max_requests;          //请求队列中允许的最大请求数
+    pthread_t* m_threads;        //描述线程池的数组，其大小为m_thread_number
+    std::list<T*> m_workqueue;   //请求队列
+    locker m_queuelocker;        //保护请求队列的互斥锁
+    sem m_queuestat;             //是否有任务需要处理
+    connection_pool* m_connPool; //数据库
+    int m_actor_model;           //模型切换
+};
+template<typename T>
+threadpool<T>::threadpool(int actor_model, connection_pool* connPool, int thread_number, 
+    int max_requests) : m_actor_model(actor_model), m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL), m_connPool(connPool){
+    if(thread_number <= 0 || max_requests <= 0)
+        throw std::exception();
+    m_threads = new pthread_t[m_thread_number];
+    if(!m_threads)
+        throw std::exception();
+    for(int i = 0; i < thread_number; ++i){
+        if(pthread_create(m_threads + i, NULL, worker, this) != 0){
+            delete[] m_threads;
+            throw std::exception();
+        }
+        if(pthread_detach(m_threads[i])){
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+}
+template<typename T>
+threadpool<T>::~threadpool(){
+    delete[] m_threads;
+}
+template<typename T>
+bool threadpool<T>::append(T* request, int state){
+    m_queuelocker.lock();
+    if(m_workqueue.size() >= m_max_requests){
+        m_queuelocker.unlock();
+        return false;
+    }
+    request->m_state = state;
+    
+}
