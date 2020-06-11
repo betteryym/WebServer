287f8607d2143c8833071c60b3290bbc00c15aa7
yym <395871036@qq.com>
2020-06-11 16:47:11 +0800

    http_conn.cpp to be continued

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -31,5 +31,121 @@ void http_conn::initmysql_result(connection_pool* connPool){
     MYSQL_RES* result = mysql_store_result(mysql);
 
     //返回结果集中的列数
-    int 
+    int num_fields = mysql_num_fields(result);
+
+    //返回所有字段结构的数组
+    MYSQL_FIELD* fields = mysql_fetch_fields(result);
+
+    //从结果集中获取下一行，将对应的用户名和密码，存入Map中
+    while(MYSQL_ROW row = mysql_fetch_row(result)){
+        string temp1(row[0]);
+        string t3mp2(row[1]);
+        users[temp1] = temp2;
+    }
 }
+
+int setnonblocking(int fd){
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+//将内核时间表注册读时间,ET模式，选择开启EPOLLONESHOT
+void addfd(int epollfd, int fd, bool one_shot, int TRIGMode){
+    epoll_event event;
+    event.data.fd = fd;
+
+    if(TRIGMode == 1)
+        event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+    else
+        event.events = EPOLLIN | EPOLLRDHUP;
+    
+    if(one_shot)
+        event.events |= EPOLLONESHOT;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+//从内核事件表删除描述符
+void removefd(int epollfd, int fd){
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    close(fd);
+}
+
+//将事件重置为EPOLLONESHOT
+void modfd(int epollfd, int fd, int ev, int TRIGMode){
+    epoll_event event;
+    event.data.fd = fd;
+
+    if(TRIGMode == 1)
+        event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+    else
+        event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+}
+
+int http_conn::m_user_count = 0;
+int http_conn::m_epollfd = -1;
+
+//关闭连接，关闭一个连接，客户总量减1；
+void http_conn::close_conn(bool real_close){
+    if(real_close && (m_sockfd != -1)){
+        printf("close %d\n", m_sockfd);
+        removefd(m_epollfd, m_sockfd);
+        m_sockfd = -1;
+        m_user_count--;
+    }
+}
+
+//初始化连接，外部调用初始化套接字地址
+void http_conn::init(int sockfd, const sockaddr_in& addr, char* root, int TRIGMode, 
+                    int close_log, string user, string passwd, string sqlname)
+{
+    m_sockfd = sockfd;
+    m_address = addr;
+
+    addfd(m_epollfd, sockfd, true, m_TRIGMode);
+    m_user_count++;
+
+    //当浏览器出现连接重置时，可能是网站根目录出错或http响应格式出错或则访问的文件中内容完全 为空
+    doc_root = root;
+    m_TRIGMode = TRIGMode;
+    m_close_log = close_log;
+
+    strcpy(sql_user, user.c_str());
+    strcpy(sql_passwd, passwd.c_str());
+    strcpy(sql_name, sqlname.c_str());
+
+    init();
+}
+
+//初始化新接受的连接
+//check_state 默认为分析请求航状态
+void http_conn::init(){
+    mysql = NULL;
+    bytes_to_send = 0;
+    bytes_have_send = 0;
+    m_check_state = CHECK_STATE_REQUESTLINE;
+    m_linger = false;
+    m_method = GET;
+    m_url = 0;
+    m_version = 0;
+    m_content_length = 0;
+    m_host = 0;
+    m_start_line = 0;
+    m_checked_idx = 0;
+    m_read_idx = 0;
+    m_write_idx = 0;
+    cgi = 0;
+    m_state = 0;
+    timer_flag = 0;
+    improv = 0;
+
+    memset(m_read_buf, '\0', READ_BUFFER_SIZE);
+    memset(m_write_buf, '\0', WRITE_BUFFER_SIZE);
+    memset(m_real_file, '\0', FILENAME_LEN);
+}
+
+//从状态机，用于分析出一行内容
+//返回值为行的读取状态，有LINE_OK, LINE_BAD, LINE_OPEN
