215ed524c1cb836be229965bde177b4f1fc2acca
betteryym <395871036@qq.com>
2020-06-15 17:12:51 +0800

    lst_timer complete

--- timer/lst_timer.cpp
+++ timer/lst_timer.cpp
@@ -128,3 +128,62 @@ void sort_timer_lst::add_timer(util_timer* timer, util_timer* lst_head){
     }
 }
 
+void Utils::init(int timeslot){
+    m_TIMESLOT = timeslot;
+}
+
+//对文件描述符设置非阻塞
+int Utils::setnonblocking(int fd){
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+//将内核事件表注册读时间、ET模式，选择开启EPOLLONESHOT
+void Utils::addfd(int epollfd, int fd, bool one_shot, int TRIGMode){
+    epoll_event event;
+    event.data.fd = fd;
+
+    if(TRIGMode == 1){
+        event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+    }
+    else{
+        event.events = EPOLLIN | EPOLLRDHUP;
+    }
+    if(one_shot)
+        event.events |= EPOLLONESHOT;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+//信号处理函数
+void Utils::sig_handler(int sig){
+    //为保证函数的可重入性，保留原来的errono
+    int save_errno = errno;
+    int msg = sig;
+    send(u_pipefd[1], (char*)&msg, 1, 0);
+    errno = save_errno;
+}
+
+//定时处理任务。重新定时以不断触发SIGALRM信号
+void Utils::timer_handler(){
+    m_timer_lst.tick();
+    alarm(m_TIMESLOT);
+}
+
+void Utils::show_error(int connfd, const char* info){
+    send(connfd, info, strlen(info), 0);
+    close(connfd);
+}
+
+int* Utils::u_pipefd = 0;
+int Utils::u_epollfd = 0;
+
+class Utils;
+void cb_func(client_data* user_data){
+    epoll_ctl(Utils::u_epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
+    assert(user_data);
+    close(user_data->sockfd);
+    http_conn::m_user_count--;
+}
